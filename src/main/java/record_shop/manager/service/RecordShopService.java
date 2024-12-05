5db560e4e391f5f54f8b1b647fcae12410a33234
Joel Cohen <jcohenf50@gmail.com>
2024-12-05 20:49:07 +0000

    feat: overridden interface methods in RecordShopServiceImpl

--- src/main/java/record_shop/manager/service/RecordShopService.java
+++ src/main/java/record_shop/manager/service/RecordShopService.java
@@ -1,18 +1,16 @@
 package record_shop.manager.service;
 
-import org.springframework.stereotype.Service;
 import record_shop.manager.model.Album;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public interface RecordShopService {
 
     List<Album> getAlbums();
     Album insertAlbum(Album album);
     Album updateAlbum(Album album);
-    Optional<Album> findAlbumById();
-    HashMap<String, Boolean> deleteAlbum();
+    Optional<Album> findAlbumById(Long id);
+    HashMap<String, Boolean> deleteAlbumById(Long id);
 }
---
+++ src/main/java/record_shop/manager/service/RecordShopServiceImpl.java
@@ -0,0 +1,56 @@
+package record_shop.manager.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
+import record_shop.manager.model.Album;
+import record_shop.manager.repository.RecordShopRepository;
+
+import java.util.*;
+
+@Service
+public class RecordShopServiceImpl implements RecordShopService {
+
+    @Autowired
+    RecordShopRepository recordShopRepository;
+
+    @Override
+    public List<Album> getAlbums() {
+        List<Album> albums = new ArrayList<>();
+        recordShopRepository.findAll().forEach(albums::add);
+        return albums;
+    }
+
+    @Override
+    public Album insertAlbum(Album album) {
+        return recordShopRepository.save(album);
+    }
+
+    @Override
+    public Album updateAlbum(Album album) {
+        Album updatedAlbum = new Album();
+        updatedAlbum.setName(album.getName());
+        updatedAlbum.setGenre(album.getGenre());
+        updatedAlbum.setYear(album.getYear());
+        updatedAlbum.setStockCount(album.getStockCount());
+
+        return updatedAlbum;
+    }
+
+    @Override
+    public Optional<Album> findAlbumById(Long id) {
+        return recordShopRepository.findById(id);
+    }
+
+    @Override
+    public HashMap<String, Boolean> deleteAlbumById(Long id) {
+        Album album = recordShopRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Album with id: " + id + " not found"));
+
+        recordShopRepository.delete(album);
+        HashMap<String, Boolean> status = new HashMap<>();
+        status.put("deleted", Boolean.TRUE);
+
+        return status;
+    }
+}
