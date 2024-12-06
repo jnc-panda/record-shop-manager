bc3f8296696a5e28af811f453b9193c9cc2cc1b5
Joel Cohen <jcohenf50@gmail.com>
2024-12-06 11:31:19 +0000

    feat: implemented test for GET mapping. Test now passing

--- src/test/java/record_shop/manager/controller/RecordShopControllerTest.java
+++ src/test/java/record_shop/manager/controller/RecordShopControllerTest.java
@@ -1,4 +1,67 @@
 package record_shop.manager.controller;
 
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import record_shop.manager.model.Album;
+import record_shop.manager.model.Artist;
+import record_shop.manager.model.Genre;
+import record_shop.manager.service.RecordShopServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@AutoConfigureMockMvc
+@SpringBootTest
 public class RecordShopControllerTest {
+
+    @Mock
+    private RecordShopServiceImpl mockRecordShopServiceImpl;
+
+    @InjectMocks
+    private RecordShopController mockRecordShopController;
+
+    @Autowired
+    private MockMvc mockMvcController;
+
+    private ObjectMapper mapper;
+
+    @BeforeEach
+    public void setup(){
+        mockMvcController = MockMvcBuilders.standaloneSetup(mockRecordShopController).build();
+        mapper = new ObjectMapper();
+    }
+
+    @Test
+    public void testGetAllBooksReturnsBooks() throws Exception {
+
+        List<Album> albums = new ArrayList<>();
+        albums.add(new Album("Paths", 2023, Genre.Electronic, 5, new Artist("G Jones")));
+        albums.add(new Album("The Dude", 1981, Genre.RnB, 11, new Artist("Quincy Jones")));
+        albums.add(new Album("Fear of the Dark", 1992, Genre.Metal, 99, new Artist("Iron Maiden")));
+
+        when(mockRecordShopServiceImpl.getAlbums()).thenReturn(albums);
+
+        this.mockMvcController.perform(
+                        MockMvcRequestBuilders.get("/api/v1/recordShop"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+//                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("Paths"))
+//                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value("The Dude"))
+//                .andExpect(MockMvcResultMatchers.jsonPath("$[2].id").value(3))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2].name").value("Fear of the Dark"));
+    }
 }
